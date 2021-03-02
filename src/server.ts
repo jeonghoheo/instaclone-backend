46fe4fe10390183187a8aa45e65125064238704a
jeonghoheo <heojeongho1992@gmail.com>
2021-03-02 19:25:54 +0900

    add - Prisma part Two

--- src/server.ts
+++ src/server.ts
@@ -15,21 +15,19 @@ const typeDefs = gql`
   }
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
   type Mutation {
     createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(title: String!): Boolean
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 const resolvers: Resolvers = {
   Query: {
     movies: () => client.movie.findMany(),
-    movie: (_, { id }) => ({
-      title: "영화",
-      year: 2021
-    })
+    movie: (_, { id }) => client.movie.findUnique({ where: { id } })
   },
   Mutation: {
     createMovie: (_, { title, year, genre }) =>
@@ -40,9 +38,9 @@ const resolvers: Resolvers = {
           genre
         }
       }),
-    deleteMovie: (_, { id }) => {
-      return true;
-    }
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({ where: { id }, data: { year } })
   }
 };
 
